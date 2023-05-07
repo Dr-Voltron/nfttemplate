f83a79ce563d91385972506f9c1fbc6de01404aa
Ojinni Toluwalope <toluwalopeojt@gmail.com>
2023-05-07 13:06:33 +0100

    NFT_MARKETPLACE FRONTEND

--- components/Navbar/Notification/Notification.jsx
+++ components/Navbar/Notification/Notification.jsx
@@ -5,10 +5,28 @@ import React from "react";
 import Style from "./Notification.module.css";
 import images from "../../../img";
 const Notification = () => {
-  return;
-  <div className={Style.notification}>
-    <p>Notification </p>
-  </div>;
+  return (
+    <div className={Style.notification}>
+      <p>Notification </p>
+      <div className={Style.notification_box}>
+        <div className={Style.notification_box_img}>
+          <Image
+            src={images.user1}
+            alt="profile image"
+            width={50}
+            height={50}
+            className={Style.notification_box_img}
+          />
+        </div>
+        <div className={Style.notication_box_info}>
+          <h4>Ojinni Toluwalope</h4>
+          <p>Measure action your user...</p>
+          <small>3 minutes ago</small>
+        </div>
+        <span className={Style.notification_box_new}> </span>
+      </div>
+    </div>
+  );
 };
 
 export default Notification;
